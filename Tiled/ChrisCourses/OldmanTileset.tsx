6c233d3e694793ad82947c4e8f0b4167a94c07fd
Petr Filaretov <p.filaretov@okko.tv>
2023-03-11 23:27:32 +0600

    new map - in progress

---
+++ Tiled/ChrisCourses/OldmanTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="OldmanSpriteSheet" tilewidth="16" tileheight="16" tilecount="28" columns="4">
+ <image source="../../tileset/NinjaAdventure-Supported/Actor/Characters/OldMan/SpriteSheet.png" width="64" height="112"/>
+</tileset>
