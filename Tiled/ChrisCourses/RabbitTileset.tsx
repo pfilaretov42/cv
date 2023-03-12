ae54ff808a1baa4d4db6fb5b7ad01d5909f0b9c0
Petr Filaretov <p.filaretov@okko.tv>
2023-03-12 16:27:28 +0600

    new map - bridges

---
+++ Tiled/ChrisCourses/RabbitTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="RabbitTileset" tilewidth="12" tileheight="12" tilecount="1" columns="1">
+ <image source="../../tileset/Animal asset pack 02 by pixel Predator/Rabbit-12x12-2.png" width="12" height="12"/>
+</tileset>
