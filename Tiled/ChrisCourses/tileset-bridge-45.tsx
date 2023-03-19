4093b9d91ed9451b2a1b9da0c8836aab6a749d57
Petr Filaretov <p.filaretov@okko.tv>
2023-03-19 16:01:02 +0600

    release

--- Tiled/ChrisCourses/tileset-bridge-45.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tileset-bridge-45" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
- <image source="../../tileset/12x12-rpg-tileset/12x12-rpg-tileset-bridge-45.png" width="384" height="384"/>
-</tileset>
