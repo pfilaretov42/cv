87e71c3ee4000a38bed557b0f63d73404a02df0a
Petr Filaretov <p.filaretov@okko.tv>
2023-04-14 22:39:29 +0400

    update texts

--- data/text.js
+++ data/text.js
@@ -6,16 +6,12 @@ const welcomeTexts = [
 
 const summaryTexts = [
     'Hi, here is a quick summary about Petr:',
-    'Senior Software Engineer with technical experience in enterprise solutions, including cloud platforms.',
-    '14 years of hands-on experience in IT.',
-    'Expert in application development using java, kotlin, and Spring.',
-    'AWS Certified Solutions Architect Associate.',
-    'Participation in the full life cycle of software development projects.',
-    'Domain areas include FinTech, Life Science, Media, Housing and Utilities Sector, Telecommunication, and Education.',
-    'Working with both SQL and NoSQL databases.',
-    'Problem isolation and analysis.',
-    'Environment and CI/CD pipeline configurations.',
-    'Learning and mastering new technologies.',
+    'Passionate software engineer with over 14 years of hands-on experience in IT focused on delivering high-quality products on time.',
+    'Known for strong technical leadership skills.',
+    'Proficient in backend development with Java, Kotlin, and Spring.',
+    'Recognized by Amazon as AWS Certified Solutions Architect Associate.',
+    'Experienced with the environment and CI/CD pipeline configurations.',
+    'Striving for excellence in every aspect of the software development lifecycle.',
 ]
 
 const skillsTexts = [
@@ -24,10 +20,8 @@ const skillsTexts = [
     'Frameworks and libraries: Spring, Spring Boot, Spring Data, Spring Security, Lombok, mapstruct, Liquibase, MyBatis, junit, Mockito, mockk, slf4j, Swagger, Google Guice.',
     'Cloud: Amazon Web Services, AWS SDK.',
     'Message brokers: Kafka, RabbitMQ.',
-    'Development infrastructure: Gradle, Maven, Jenkins, Git, SVN, spotbugs, checkstyle, jacoco, SonarQube, Docker, Kubernetes, Helm, Prometheus, Grafana, BlackDuck, Veracode.',
+    'Development infrastructure: Gradle, Maven, Jenkins, Git, SVN, spotbugs, checkstyle, jacoco, SonarQube, detekt, Docker, Kubernetes, Helm, Prometheus, Grafana, BlackDuck, Veracode.',
     'Databases: PostgreSQL, Oracle, Neo4j.',
-    'IDE: IntelliJ IDEA, Eclipse.',
-    'Modelling systems: draw.io, Microsoft Visio.',
     'Methodologies: Scrum, Kanban.',
 ]
 
@@ -49,18 +43,18 @@ const contactsTexts = [
 
 const okkoTexts = [
     "Since June 2022 till now Petr is working at&nbsp;<a href=\"https://okko.tv\" target=\"_blank\" rel=\"noopener noreferrer\">Okko</a>.",
-    'Contribution: technical leadership of 2 projects.',
-    'Environment: Java 11, Kotlin, Spring, Spring Boot, RabbitMQ, Kafka, Neo4j, PostgreSQL, Gradle, Docker, Helm, Kubernetes, Jenkins.',
+    'TL;DR: technical leadership of 2 projects.',
+    'Environment: Java 11/17, Kotlin, Spring, Spring Boot, RabbitMQ, Kafka, Neo4j, PostgreSQL, ClickHouse, Gradle, Docker, Helm, Kubernetes, Vault, Jenkins.',
 ]
 
 const epamTexts = [
     "From August 2017 to June 2022 Petr worked at&nbsp;<a href=\"https://epam.com\" target=\"_blank\" rel=\"noopener noreferrer\">EPAM</a>.",
-    'Contribution: technical leadership of 6 projects, developer in 1 project.',
-    'Environment: Java 8/11, Groovy, Spring, Spring Boot, RabbitMQ, AWS, Oracle, Gradle, Maven, Docker, Helm, Kubernetes, Jenkins, SonarQube.',
+    'TL;DR: technical leadership of 6 projects, developer in 1 project.',
+    'Environment: Java 8/11/17, Groovy, Spring, Spring Boot, AWS, RabbitMQ, Oracle, Gradle, Docker, Helm, Kubernetes, Jenkins, SonarQube.',
 ]
 
 const bccTexts = [
     "From September 2008 to August 2017 Petr worked at&nbsp;<a href=\"https://bcc.ru\" target=\"_blank\" rel=\"noopener noreferrer\">bcc</a>.",
-    'Contribution: technical leadership of 1 project, developer in 2 projects.',
+    'TL;DR: technical leadership of 1 project, developer in 2 projects.',
     'Environment: Java 6/7, Java EE, Java Servlets, JSP, Spring, HornetQ, PostgreSQL, Oracle, Apache Tiles, Maven, TeamCity, Apache Tomcat, JBoss.'
 ]
