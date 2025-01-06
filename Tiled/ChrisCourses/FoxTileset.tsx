f16f0b1ee124e0795164b4a0950bd482573dc18f
pfilaretov42 <petr.filaretov@gmail.com>
2025-01-05 20:39:15 -0800

    add missing png-sources for tiles

--- Tiled/ChrisCourses/FoxTileset.tsx
+++ Tiled/ChrisCourses/FoxTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="FoxTileset" tilewidth="12" tileheight="12" tilecount="4" columns="2">
- <image source="../../tileset/Animal asset pack 02 by pixel Predator/Fox-12x12.png" width="24" height="24"/>
+<tileset version="1.11" tiledversion="1.11.0" name="FoxTileset" tilewidth="12" tileheight="12" tilecount="4" columns="2">
+ <image source="../../tileset/Animal-asset-pack-02-by-pixel-Predator/Fox-24x24.png" width="24" height="24"/>
 </tileset>
